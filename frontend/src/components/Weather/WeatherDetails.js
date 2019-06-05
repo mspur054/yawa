48bfa83d57164f7ff36dae31892b0c1b6a2d37f3
mspur054 <mspur054@uottawa.ca>
2019-06-04 21:58:16 -0400

    added apparent temp to current page

--- frontend/src/components/Weather/WeatherDetails.js
+++ frontend/src/components/Weather/WeatherDetails.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
-import { convertToMph } from "../../helpers";
+import { convertToMph, convertToF } from "../../helpers";
 
 const WeatherDetails = ({ data, units }) => {
-  const { precipProbability, humidity, windSpeed } = data.currently;
+  const {
+    precipProbability,
+    humidity,
+    windSpeed,
+    apparentTemperature
+  } = data.currently;
 
   function renderWind() {
     const metricWind = (windSpeed * 3.6).toFixed(1);
@@ -16,6 +21,12 @@ const WeatherDetails = ({ data, units }) => {
   return (
     <React.Fragment>
       <Typography>DETAILS</Typography>
+
+      <Typography>{`Feels Like: ${Math.floor(
+        units === "METRIC"
+          ? apparentTemperature
+          : convertToF(apparentTemperature)
+      )}°${units === "METRIC" ? "C" : "F"}`}</Typography>
       <Typography>{`Precipitation: ${precipProbability}%`}</Typography>
       <Typography>{`Humidity: ${Math.round(humidity * 100)}%`}</Typography>
       <Typography>{`Wind: ${renderWind()}`}</Typography>
